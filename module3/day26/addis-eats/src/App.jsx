67e5da1d9457bd6d09b8a9381a5df73e55485ada
Yeabkk <yeabsera0809@gmail.com>
2026-09-01 23:49:24 +0300

    day 23 exerise done

---
+++ module3/day26/addis-eats/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './app.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1>Welcome to React</h1>
+    <p>first try</p>
+   </div>
+  )
+}
+
+export default App
